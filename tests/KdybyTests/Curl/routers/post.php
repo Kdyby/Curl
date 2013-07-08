dbf090e8799e98c3b91b66fa9adff3d6c99aa6d2
Filip Procházka <filip@prochazka.su>
2013-07-08 20:03:56 +0200

    tests are now self-sufficient, thanks to php build-in webserver

---
+++ tests/KdybyTests/Curl/routers/post.php
@@ -0,0 +1,5 @@
+<?php
+
+header('Content-Type: text/plain; charset=utf-8');
+print_r($_POST);
+print_r($_FILES);
--- tests/KdybyTests/bootstrap.php
+++ tests/KdybyTests/bootstrap.php
@@ -40,3 +40,84 @@ function id($val) {
 function run(Tester\TestCase $testCase) {
 	$testCase->run(isset($_SERVER['argv'][1]) ? $_SERVER['argv'][1] : NULL);
 }
+
+
+
+class HttpServer extends Nette\Object
+{
+
+	/**
+	 * @var array
+	 */
+	private $pipes = array();
+
+	/**
+	 * @var resource
+	 */
+	private $process;
+
+	/**
+	 * @var string
+	 */
+	private $router;
+
+	private static $spec = array(
+		0 => array("pipe", "r"), // stdin is a pipe that the child will read from
+		1 => array("pipe", "w"), // stdout is a pipe that the child will write to
+		2 => array("pipe", "w"), // errors
+	);
+
+	public function __construct($router)
+	{
+		$this->router = $router;
+	}
+
+	public function __destruct()
+	{
+		$this->murder();
+	}
+
+	public function start($port = NULL, $ip = '127.0.0.1')
+	{
+		$this->murder();
+
+		$port = $port ? : rand(8000, 10000);
+		$cmd = sprintf('php -S %s:%d %s', $ip, $port, escapeshellarg($this->router));
+		if (!is_resource($this->process = proc_open($cmd, self::$spec, $this->pipes))) {
+			throw new \RuntimeException("Could not execute: `$cmd`");
+		}
+
+		sleep(1); // give him some time to boot up
+
+		return 'http://' . $ip .':' . $port;
+	}
+
+	public function murder()
+	{
+		if (!is_resource($this->process)) {
+			return;
+		}
+
+		$status = proc_get_status($this->process);
+		if ($status['running'] == true) {
+			fclose($this->pipes[1]); //stdout
+			fclose($this->pipes[2]); //stderr
+
+			//get the parent pid of the process we want to kill
+			$pPid = $status['pid'];
+
+			//use ps to get all the children of this process, and kill them
+			foreach (array_filter(preg_split('/\s+/', `ps -o pid --no-heading --ppid $pPid`)) as $pid) {
+				if (is_numeric($pid)) {
+					posix_kill($pid, 9); // SIGKILL signal
+				}
+			}
+		}
+
+		fclose($this->pipes[0]);
+		proc_close($this->process);
+
+		$this->process = NULL;
+	}
+
+}
